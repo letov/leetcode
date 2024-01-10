6eedf40e1160a0fde5c89505e2171974d58cff6a
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 16:44:42 +0300

    init: median-of-two-sorted-arrays

---
+++ tasks/median-of-two-sorted-arrays.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+var findMedianSortedArrays = function(nums1, nums2) {
+
+};
+
+console.log(findMedianSortedArrays([1,2], [3,4]));
