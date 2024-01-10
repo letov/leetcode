629de4ae8e518402cd081833789d7e9cd77d8376
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 17:38:37 +0300

    init: median-of-two-sorted-arrays

--- tasks/median-of-two-sorted-arrays.js
+++ tasks/median-of-two-sorted-arrays.js
@@ -3,9 +3,15 @@
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+// O(log(n+m))
+
+// [1,4,7,90] [5,7,8]
+//  ^          ^
+// [1,4,7,90,5,7,8]
+//  ^
 
 var findMedianSortedArrays = function(nums1, nums2) {
-
+  // TODO: ???
 };
 
 console.log(findMedianSortedArrays([1,2], [3,4]));
