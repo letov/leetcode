dc8b65fa71815a3dd9b2a5f7324e19abd052e0d1
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 22:51:37 +0300

    done: reverse-integer

--- tasks/reverse-integer.js
+++ tasks/reverse-integer.js
@@ -1,7 +1,12 @@
 // https://leetcode.com/problems/reverse-integer/
 
-var reverse = function(x) {
+// Input: x = 123
+// Output: 321
 
+var reverse = function(x) {
+    const zn = x < 0 ? -1 : 1;
+    const result = zn * +Math.abs(x).toString().split('').reverse().join('');
+    return result < -Math.pow(2, 31) || result >= Math.pow(2, 31) ? 0 : result;
 };
 
-console.log(reverse(120));
+console.log(reverse(1534236469));
