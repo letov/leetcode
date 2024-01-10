2e45a5edb28fdb609aa277a6bbd5be408fd2745f
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 20:04:34 +0300

    init: zigzag-conversion

---
+++ tasks/zigzag-conversion.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/zigzag-conversion/
+
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
+var convert = function(s, numRows) {
+
+};
+
+console.log(convert("PAYPALISHIRING", 4));
