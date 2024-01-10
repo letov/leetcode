6266cb727b1a1c486dce13dd12b1758180101331
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 19:59:50 +0300

    done: longest-palindromic-substring

--- tasks/longest-palindromic-substring.js
+++ tasks/longest-palindromic-substring.js
@@ -4,8 +4,51 @@
 // Output: "bab"
 // Explanation: "aba" is also a valid answer.
 
-var longestPalindrome = function(s) {
+// aba
+// ^
+//   ^
 
+const isPalindrome = (s) => {
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false;
+        }
+        j--;
+        i++;
+    }
+    return true;
 };
 
-console.log(longestPalindrome("cbbd"));
+var longestPalindrome = function(s) {
+    let i = 0;
+    let result = '';
+
+    if (s.length === 1) {
+        return s;
+    }
+
+    while(i <= s.length - 2) {
+        if (s.length - i <= result.length) {
+            break;
+        }
+        let j = i + 1;
+        let current = s[i];
+        while(j <= s.length - 1) {
+            result = current.length > result.length && isPalindrome(current)
+                ? current
+                : result;
+            current += s[j];
+            j++;
+        }
+        result = current.length > result.length && isPalindrome(current)
+            ? current
+            : result;
+        i++;
+    }
+
+    return result;
+};
+
+console.log(longestPalindrome("adasa"));
