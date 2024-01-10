4a163c25a99499e94d69fe28ce01073fb0a5ef9c
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 14:40:20 +0300

    init: longest-substring-without-repeating-characters

---
+++ tasks/longest-substring-without-repeating-characters.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+var lengthOfLongestSubstring = function(s) {
+
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
