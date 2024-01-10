bff9a765bdd8268a3486223ab68270be54f85cfd
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-10 17:40:20 +0300

    init: longest-palindromic-substring

---
+++ tasks/longest-palindromic-substring.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+var longestPalindrome = function(s) {
+
+};
+
+console.log(longestPalindrome("cbbd"));
