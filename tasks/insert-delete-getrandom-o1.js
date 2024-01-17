e62cb518316e80d36025eeb149e65811c5f8e7dd
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-01-17 17:45:00 +0300

    init: insert-delete-getrandom-o1

---
+++ tasks/insert-delete-getrandom-o1.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+
+var RandomizedSet = function() {
+
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+
+// console.log();
